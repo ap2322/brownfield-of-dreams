a2548720ac89cc68425ecc9e0b42c7147f42a179
Alice Post <alicep2322@gmail.com>
2019-12-03 17:40:03 -0700

    Update factory to include thumbnail per validations
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     title { Faker::Games::Pokemon.name }
     description { Faker::TvShows::SiliconValley.motto }
     video_id { Faker::Crypto.md5 }
+    thumbnail { "https://i.ytimg.com/vi/XsPVWGKK0qI/hqdefault.jpg" }
     tutorial
   end
 end
