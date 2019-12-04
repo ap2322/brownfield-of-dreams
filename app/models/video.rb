5d2e94697c85dd080d3cbb5338f631965f7bc0fb
Alice Post <alicep2322@gmail.com>
2019-12-03 17:38:25 -0700

    Make test and model methods to increment video.position after create
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,15 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  validates_presence_of :title, :description, :video_id, :thumbnail
+
+  after_create :check_position
+
+  private
+  def check_position
+    if self.position == 0
+      self.update_attribute(:position, self.tutorial.video_count)
+    end
+  end
 end
