f6b840031f4b5cb5c963e24f7eabf8c2f4dae2d0
Alice Post <alicep2322@gmail.com>
2019-12-04 18:00:09 -0700

    Stub omniauth in config file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,24 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], scope: 'repo'
+  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], scope: "user,repo"
+
+  def stub_omniauth
+    # first, set OmniAuth to run in test mode
+    OmniAuth.config.test_mode = true
+    # then, provide a set of fake oauth data that
+    # omniauth will use when a user tries to authenticate:
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github',
+      extra: {
+        raw_info: {
+          user_id: "1234",
+          name: "Horace",
+        }
+      },
+      credentials: {
+        token: "pizza",
+        secret: "secretpizza"
+      }
+    })
+  end
 end
