2010ee6ba9b30ce57c3d5bcf83a6db80224ff22b
Alice Post <alicep2322@gmail.com>
2019-12-04 23:59:40 -0700

    Use omniauth to connect, then call 3 github endpoints, render new objects on dashboard

--- app/controllers/users/github_info_controller.rb
+++ app/controllers/users/github_info_controller.rb
@@ -1,27 +1,45 @@
 class Users::GithubInfoController < ApplicationController
   def show
     github_info = request.env['omniauth.auth']
+    user = current_user
 
-    token = github_info[:credentials][:token]
-    username = github_info[:info][:nickname]
+    facade = GithubInfoFacade.new(github_info, user)
 
-
-    conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers["token"] = token
-      faraday.adapter Faraday.default_adapter
-    end
-
-    repos = conn.get("/users/#{username}/repos")
-    repos_as_json = JSON.parse(repos.body)
-
-    followers = conn.get("/users/#{username}/followers")
-    followers_as_json = JSON.parse(followers.body)
-
-    following = conn.get("/users/#{username}/following")
-    following_as_json = JSON.parse(following.body)
+    # want an array of objects for repos, followers, and following
+    repos = facade.repos
+    # followers = facade.followers
+    # followers = facade.following
+    # declaring what we wish existed
+    render 'users/show', locals: {
+      github_info_for_user: GithubInfoFacade.new(github_info, user)
+    }
 
 
-    binding.pry
+    #
+    # github_info = request.env['omniauth.auth']
+    # token = github_info[:credentials][:token]
+    # username = github_info[:info][:nickname]
+    #
+    # user.username = username
+    # user.uid      = github_info[:uid]
+    # user.token    = token
+    # user.save
+    #
+    #
+    #
+    # conn = Faraday.new(url: "https://api.github.com") do |faraday|
+    #   faraday.headers["token"] = token
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # repos = conn.get("/users/#{username}/repos")
+    # repos_as_json = JSON.parse(repos.body)
+    #
+    # followers = conn.get("/users/#{username}/followers")
+    # followers_as_json = JSON.parse(followers.body)
+    #
+    # following = conn.get("/users/#{username}/following")
+    # following_as_json = JSON.parse(following.body)
 
   end
 end
