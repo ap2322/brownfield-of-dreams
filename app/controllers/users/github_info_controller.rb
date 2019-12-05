e2ccb09f7905a016ff54c64ddc87c971b20de0d9
Alice Post <alicep2322@gmail.com>
2019-12-04 17:59:43 -0700

    We use the token to hit the github api endpoint to get the repo data

--- app/controllers/users/github_info_controller.rb
+++ app/controllers/users/github_info_controller.rb
@@ -3,8 +3,20 @@ class Users::GithubInfoController < ApplicationController
     github_info = request.env['omniauth.auth']
 
     token = github_info[:credentials][:token]
+    username = github_info[:info][:nickname]
 
 
+    conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["token"] = token
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("https://api.github.com/users/#{username}/repos")
+
+    repos = JSON.parse(response.body)
     binding.pry
+
+
+
   end
 end
