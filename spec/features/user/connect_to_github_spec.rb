e76cef595b5bdf44b5616a82e409c4a5bd79b126
Alice Post <alicep2322@gmail.com>
2019-12-06 16:19:51 -0700

    Add tests with vcr to connect to github and display repos
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/features/user/connect_to_github_spec.rb
+++ spec/features/user/connect_to_github_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'user can be authenticated and grant acces via github' do
-  it 'authenticates user and fetches user info from github' do
+  it 'authenticates user and fetches user info from github', :vcr do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     stub_omniauth
@@ -18,5 +18,21 @@ describe 'user can be authenticated and grant acces via github' do
     within('.github_info') do
       expect(page).to have_content(user.username)
     end
+  end
+
+  it 'displays five of the users repos', :vcr do
+    user = create(:user, token: ENV['GITHUB_TEST_TOKEN'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/dashboard'
+    within '.repos' do
+      expect(page).to have_css('#repo-0')
+      expect(page).to have_css('#repo-1')
+      expect(page).to have_css('#repo-2')
+      expect(page).to have_css('#repo-3')
+      expect(page).to have_css('#repo-4')
+    end
+
+    expect(page.find('#repo-0')).to_not be_nil
+
   end
 end
