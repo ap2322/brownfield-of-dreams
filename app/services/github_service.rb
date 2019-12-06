f2afdccc8005271161833d2c027b9190d91c40d9
Mary Lang <mcl86@cornell.edu>
2019-12-06 16:46:04 -0700

    Add get followers to service
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,11 +1,19 @@
 class GithubService
+
   def repos_json(token)
     connection = conn(token)
     repos = connection.get("/user/repos")
     JSON.parse(repos.body)
   end
 
+  def followers_json(token)
+    connection = conn(token)
+    followers = connection.get("/user/followers")
+    JSON.parse(followers.body)
+  end
+
   private
+
   def conn(token)
     Faraday.new(url:"https://api.github.com") do |f|
       f.params['access_token'] = token
