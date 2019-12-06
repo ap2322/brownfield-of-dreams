da97b0f82c2fff48ccc2e0b53724abbb4d2fe495
Alice Post <alicep2322@gmail.com>
2019-12-06 16:19:08 -0700

    Filter sensitive data in vcr
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<GITHUB_TEST_TOKEN>") { ENV['GITHUB_TEST_TOKEN'] }
 end
 
 
