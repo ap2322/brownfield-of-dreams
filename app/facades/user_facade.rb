37f206f7cecc80f2d54a34a450ab4f9a97a7eec8
Alice Post <alicep2322@gmail.com>
2019-12-06 16:25:00 -0700

    Define repos method, GithubService class, and Repository class in user_facade
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -2,5 +2,14 @@ class UserFacade < SimpleDelegator
   def initialize(user)
     super(user)
   end
-  
+
+  def repos
+    service = GithubService.new
+    @repos ||= service.repos_json(token)
+
+    @repos.map do |repo|
+      Repository.new(repo)
+    end[0..4]
+  end
+
 end
