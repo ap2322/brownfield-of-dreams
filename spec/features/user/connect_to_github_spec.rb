96d16ab8d2f01138ff8e39de415bdf4faf4fd554
Mary Lang <mcl86@cornell.edu>
2019-12-06 16:57:19 -0700

    Add to connect to github spec for followings
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-Authored-By: Alice Post <alicep2322@gmail.com>

--- spec/features/user/connect_to_github_spec.rb
+++ spec/features/user/connect_to_github_spec.rb
@@ -50,4 +50,20 @@ describe 'user can be authenticated and grant acces via github' do
     end
     expect(page.find('#follower-0')).to_not be_nil
   end
+
+  it 'displays user following', :vcr do
+    user = create(:user, token: ENV['GITHUB_TEST_TOKEN'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    within '.followings' do
+      expect(page).to have_css('#following-0')
+      expect(page).to have_css('#following-1')
+      expect(page).to have_css('#following-2')
+      expect(page).to have_css('#following-3')
+      expect(page).to have_css('#following-4')
+    end
+    expect(page.find('#following-0')).to_not be_nil
+  end
 end
