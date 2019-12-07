51490b0a5eb4b9c60e6f000f7a562c065482f7ca
Alice Post <alicep2322@gmail.com>
2019-12-07 16:18:54 -0700

    Move visitor test to spec/visitors and remove obsolete test
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/features/user/user_can_bookmark_a_video_spec.rb
+++ spec/features/user/user_can_bookmark_a_video_spec.rb
@@ -31,20 +31,4 @@ describe 'A registered user' do
     click_on 'Bookmark'
     expect(page).to have_content("Already in your bookmarks")
   end
-
-  it "shows tooltip for visitor not logged-in on hover to tell them they'll need to log in", :js do
-    tutorial= create(:tutorial)
-    video = create(:video, tutorial_id: tutorial.id)
-
-    visit tutorial_path(tutorial)
-    driver = Selenium::WebDriver.for :chrome
-
-    element = page.driver.browser.find_element(:css, "a#bookmark-btn")
-    page.driver.browser.action.move_to(element).perform
-    expect(page.find(".tooltip-toggle").visible?).to eq(true)
-
-    click_link 'Bookmark'
-
-    expect(current_path).to eq("/login")
-  end
 end
