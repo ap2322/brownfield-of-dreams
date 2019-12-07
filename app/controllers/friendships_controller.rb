f98e73eae4ebc36d40038415b332e3c54c4a71a8
Mary Lang <mcl86@cornell.edu>
2019-12-07 12:21:23 -0700

    Add friendship model and controller
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,16 @@
+class FriendshipsController < ApplicationController
+
+  def create
+    friendship = current_user.friendships.create(friend_id: params[:friend_id])
+
+    if friendship.save
+      flash[:success] = 'Added friend!'
+    else
+      flash[:error] = 'Unable to add friend'
+    end
+    redirect_to '/dashboard'
+  end
+
+
+
+end
