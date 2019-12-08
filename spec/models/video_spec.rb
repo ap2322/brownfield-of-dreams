d0331cb09f1bd1317fdb1a9c01ae7b950ca1b9fa
Mary Lang <mcl86@cornell.edu>
2019-12-08 10:49:40 -0700

    Add relationship tests to video spec
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe Video, type: :model do
 
   describe 'relationships' do
     it { should belong_to :tutorial }
+    it { should have_many :user_videos }
+    it { should have_many(:users).through(:user_videos) }
   end
 
   describe 'callbacks' do
