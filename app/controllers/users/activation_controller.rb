dc4c1dd3b83206e57f615d3e17f9590ac7feea6c
Mary Lang <mcl86@cornell.edu>
2019-12-08 13:43:54 -0700

    Add resend link and action for activate account
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- app/controllers/users/activation_controller.rb
+++ app/controllers/users/activation_controller.rb
@@ -2,9 +2,19 @@ class Users::ActivationController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    user.toggle(active?)
+    user.toggle(:active?)
 
     redirect_to "/users/#{user.id}/activation/activated"
+  end
+
+  def show
+  end
+
+  def new
+    user = User.find(params[:id])
+    ActivationMailer.activation(user).deliver_now
+
+    redirect_to '/dashboard'
   end
 
 end
