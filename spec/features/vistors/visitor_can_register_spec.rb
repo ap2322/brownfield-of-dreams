170e1d6be49c3abaed7d220587c5f46cc80339d8
Alice Post <alicep2322@gmail.com>
2019-12-08 15:01:38 -0700

    Add test for new user to receive email to activate their account
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
-describe 'vister can create an account', :js do
+describe 'vister can create an account', :vcr, :js do
+  before(:each) do
+    ActionMailer::Base.deliveries = []
+  end
+
   it ' visits the home page' do
     email = 'jimbob@aol.com'
     first_name = 'Jim'
@@ -33,7 +37,9 @@ describe 'vister can create an account', :js do
     expect(page).to have_content(last_name)
     expect(page).to_not have_content('Sign In')
   end
+
   it 'visits home page to register and activate via email' do
+
     email = 'jimbob@aol.com'
     first_name = 'Jim'
     last_name = 'Bob'
@@ -51,16 +57,23 @@ describe 'vister can create an account', :js do
 
     click_on'Create Account'
 
+    user = User.last
+
     expect(current_path).to eq('/dashboard')
     expect(page).to have_content('Logged in as Jim')
     expect(page).to have_content('This account has not yet been activated. Please check your email.')
 
-    # MOCK ACTIVATE
+    last_email = ActionMailer::Base.deliveries.last
+    expect(last_email.to).to include(email)
+    expect(last_email.body).to have_content("Visit here to activate your account.")
+    expect(last_email.body).to have_link("Activate Account")
 
-    # CAN WE VISIT CONFIRMATION PAGE?
+    visit "/users/#{user.id}/activation"
+
     expect(page).to have_content("Thank you! Your account is now activated.")
 
     visit '/dashboard'
     expect(page).to have_content('Status: Active')
+    expect(page).to_not have_content('This account has not yet been activated. Please check your email.')
   end
 end
