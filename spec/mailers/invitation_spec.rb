21b0ddcc827be320f4fd032bf98d427f01ef9fda
Mary Lang <mcl86@cornell.edu>
2019-12-09 19:36:10 -0700

    Add mailer invitation spec
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- spec/mailers/invitation_spec.rb
+++ spec/mailers/invitation_spec.rb
@@ -1,5 +1,26 @@
 require "rails_helper"
 
 RSpec.describe InvitationMailer, type: :mailer do
+  before(:each) do
+    @user = create(:user, email: 'testing@testing.com', github_name: 'Turing')
+    @friend = GithubUser.new({'name' => 'Alice', 'login' => 'ap232', 'html_url' => 'github.com/ap232', 'email' => 'ap232@brownfield.com'})
+  end
 
+  describe 'sends activation email' do
+    let(:mail) {InvitationMailer.invite(@user, @friend)}
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq("You're Invited!")
+      expect(mail.to).to eq(['ap232@brownfield.com'])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it 'renders the body with an activation link' do
+      expect(mail.body.encoded).to match("Hello Alice")
+      expect(mail.body.encoded).to match("#{@user.github_name} has invited you to join Brownfield MA!")
+      expect(mail.body.encoded).to match("You can create an account")
+      expect(mail.body.encoded).to have_link("here")
+    end
+
+  end
 end
