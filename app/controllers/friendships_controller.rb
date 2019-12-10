0adf5cd6e6cda77132f81d037bf8dafad742ba0c
Mary Lang <mcl86@cornell.edu>
2019-12-10 13:05:45 -0700

    Add validation to friendship controller
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
     friendship = current_user.friendships.new(friend_id: params[:friend_id])
-    if friendship.save
+    if friendship.save && friendship.validate_friend 
       flash[:success] = 'Friend added!'
     else
       flash[:error] = 'Unable to add friend'
