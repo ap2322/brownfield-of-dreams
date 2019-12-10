633916ede2fc98b5bc7850d971b54b7967c3e899
Alice Post <alicep2322@gmail.com>
2019-12-09 17:11:28 -0700

    Specific tests for ActivationMailer
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/mailers/activation_spec.rb
+++ spec/mailers/activation_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 RSpec.describe ActivationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = create(:user, email: 'testing@testing.com')
+  end
+
+  describe 'sends activation email' do
+    let(:mail) {ActivationMailer.activation(@user)}
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq("Turing Tutorials Account Activation")
+      expect(mail.to).to eq(["testing@testing.com"])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it 'renders the body with an activation link' do
+      expect(mail.body.encoded).to match("Visit here to activate your account.")
+      expect(mail.body.encoded).to match("Activate Account")
+    end
+
+  end
 end
