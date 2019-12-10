433631765e80078c69a22e9d510b8a2110219b94
Mary Lang <mcl86@cornell.edu>
2019-12-10 13:05:45 -0700

    Add cannot reuse email to visitor register spec
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'vister can create an account', :vcr, :js do
+describe 'visitor can create an account', :vcr, :js do
   before(:each) do
     ActionMailer::Base.deliveries = []
   end
@@ -36,6 +36,38 @@ describe 'vister can create an account', :vcr, :js do
     expect(page).to have_content(first_name)
     expect(page).to have_content(last_name)
     expect(page).to_not have_content('Sign In')
+  end
+
+  it 'cannot reuse email' do
+    email = 'jimbob@aol.com'
+    first_name = 'Jim'
+    last_name = 'Bob'
+    password = 'password'
+    password_confirmation = 'password'
+
+    visit '/'
+
+    click_on 'Register'
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+    click_on'Create Account'
+
+    click_on 'Log Out'
+
+    click_on 'Register'
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: 'faker'
+    fill_in 'user[last_name]', with: 'faker'
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+    click_on'Create Account'
+
+    expect(page).to have_content('Email already registered')
   end
 
   it 'visits home page to register and activate via email' do
