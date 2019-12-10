f52a843b826959c877f151e5cbab529d53a4aa86
Mary Lang <mcl86@cornell.edu>
2019-12-09 19:14:13 -0700

    Add specs for invitation
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

---
+++ spec/features/user/user_can_invite_friends_to_app_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+
+describe 'user can invite friends' do
+  before(:each) do
+    ActionMailer::Base.deliveries = []
+  end
+
+  it 'invite link from dashboard sends email invite' do
+    WebMock.enable_net_connect!
+    VCR.eject_cassette
+    VCR.turn_off!(ignore_cassettes: true)
+    user_1 = create(:user, token: ENV['GITHUB_TEST_TOKEN'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit '/dashboard'
+
+    click_link 'Send Invite'
+
+    expect(current_path).to eq('/invite')
+
+    fill_in 'Github handle', with: 'turingschool'
+    click_on 'Send Invite'
+
+    last_email = ActionMailer::Base.deliveries.last
+    expect(last_email.to).to include('contact@turing.io')
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content('Successfully sent invite!')
+  end
+  it 'invite link from dashboard will not send email if github user does not have email' do
+    WebMock.enable_net_connect!
+    VCR.eject_cassette
+    VCR.turn_off!(ignore_cassettes: true)
+    user_1 = create(:user, token: ENV['GITHUB_TEST_TOKEN'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit '/dashboard'
+
+    click_link 'Send Invite'
+
+    expect(current_path).to eq('/invite')
+
+    fill_in 'Github_handle', with: 'johnktravers'
+    click_on 'Send Invite'
+
+    expect(ActionMailer::Base.deliveries).to eq([])
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+  end
+end
