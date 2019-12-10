518ce0a44ca8f9c6dfb4636185141c5a24b647d4
Alice Post <alicep2322@gmail.com>
2019-12-10 16:40:11 -0700

    Add model test for dependent destroy for tutorial->videos->user_videos
    Co-Authored-By: Mary Lang <mcl86@cornell.edu>

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -8,7 +8,26 @@ RSpec.describe Tutorial, type: :model do
   end
   describe 'relationships' do
     it { should have_many :videos }
+
+    it 'deletes videos when a tutorial is deleted' do
+      user = create(:user)
+      tutorial = create(:tutorial)
+      video_1 = create(:video, tutorial_id: tutorial.id)
+      video_2 = create(:video, tutorial_id: tutorial.id)
+      video_3 = create(:video, tutorial_id: tutorial.id)
+      user_video_1 = user.user_videos.create(video_id: video_3.id)
+
+      tutorial.destroy
+      no_videos = tutorial.videos
+
+      expect(no_videos).to eq([])
+      expect{ Video.find(video_1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect{ Video.find(video_2.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect{ Video.find(video_3.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      expect{ UserVideo.find(user_video_1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
+
   describe 'instance methods' do
     it 'video_count' do
       tutorial = create(:tutorial)
