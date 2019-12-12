2f75d742c23c88efddef4c22a5bdad51270bbbfd
Alice Post <alicep2322@gmail.com>
2019-12-11 18:16:32 -0700

    Add method to check if user and friend_id are connected on Github

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,4 +5,9 @@ class Friendship < ApplicationRecord
   def validate_friend
     User.ids.include?(friend_id)
   end
+
+  def github_connection?(user, friend_id)
+    friend_handle = User.select(:github_name).find(friend_id)
+    user.user_followers.include?(friend_handle.github_name) && user.user_followings.include?(friend_handle.github_name)
+  end
 end
