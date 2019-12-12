d34f02fbc35f4626eb849b9d31605c2eb7e979e4
Alice Post <alicep2322@gmail.com>
2019-12-12 13:50:19 -0700

    Rubocop styling
    Co-Authored-By: Mary Lang <mcl86@cornell.edu>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,9 @@ class FriendshipsController < ApplicationController
   def create
     friendship = current_user.friendships.new(friend_id: params[:friend_id])
 
-    if friendship.validate_friend && friendship.github_connection?(current_user, params[:friend_id]) && friendship.save
+    if friendship.validate_friend &&
+       friendship.github_connection?(current_user, params[:friend_id]) &&
+       friendship.save
       flash[:success] = 'Friend added!'
     else
       flash[:error] = 'Unable to add friend'
