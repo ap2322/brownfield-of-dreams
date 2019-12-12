ae4cb830f37e9c58d97285106a987fb9722e3a57
Alice Post <alicep2322@gmail.com>
2019-12-12 14:29:17 -0700

    Move making tutorial and videos to PlaylistObject.rb
    Co-Authored-By: Mary Lang <mcl86@cornell.edu>

--- app/controllers/admin/playlist_tutorials_controller.rb
+++ app/controllers/admin/playlist_tutorials_controller.rb
@@ -1,53 +1,23 @@
 class Admin::PlaylistTutorialsController < ApplicationController
-
   def new
   end
 
   def create
-    service = YoutubeService.new
-    tutorial_info = service.playlist_tutorial_info(playlist_params[:playlist_id])
-
-    tutorial = Tutorial.new({
-      title: tutorial_info[:items][0][:snippet][:title],
-      description: description(tutorial_info[:items][0][:snippet][:description]),
-      thumbnail: tutorial_info[:items][0][:snippet][:thumbnails][:default][:url],
-      playlist_id: playlist_params[:playlist_id]
-      })
-
+    playlist_object = PlaylistObjects.new(playlist_params[:playlist_id])
+    tutorial = playlist_object.tutorial
     if tutorial.save
-      make_videos(tutorial)
-      flash[:success] = "Successfully created tutorial. #{view_context.link_to('View it here', tutorial_url(tutorial))}."
+      playlist_object.make_videos
+      flash[:success] = "Successfully created tutorial. " +
+                        "#{view_context.link_to('View it here', tutorial_url(tutorial))}."
     else
       flash[:error] = 'Unable to create tutorial.'
     end
     redirect_to '/admin/dashboard'
   end
 
-
-
-  def make_videos(tutorial)
-    service = YoutubeService.new
-    videos_info = service.playlist_videos_info(playlist_params[:playlist_id])
-    videos_info[:items].each do |video_info|
-      tutorial.videos.create({
-        title: video_info[:snippet][:title],
-        description: video_info[:snippet][:description],
-        thumbnail: video_info[:snippet][:thumbnails][:default][:url],
-        video_id: video_info[:snippet][:resourceId][:videoId],
-        position: video_info[:snippet][:position]
-        })
-    end
-  end
-
   private
-
-  def description(info)
-    return 'unknown' unless info.length > 0
-    info
-  end
 
   def playlist_params
     params.require(:playlist_tutorials).permit(:playlist_id)
   end
-
 end
