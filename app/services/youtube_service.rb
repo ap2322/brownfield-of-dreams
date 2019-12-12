de867062102a9da9b64eed7b9e5e4858d11adb9f
Mary Lang <mcl86@cornell.edu>
2019-12-12 11:38:12 -0700

    Add to youtube service for import playlist
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -5,10 +5,23 @@ class YoutubeService
     get_json("youtube/v3/videos", params)
   end
 
+
+  def playlist_videos_info(id)
+    params = { part: 'snippet', playlistId: id }
+
+    get_json('youtube/v3/playlistItems', params)
+  end
+
+  def playlist_tutorial_info(id)
+    params = { part: 'snippet', id: id }
+
+    get_json('youtube/v3/playlists', params)
+  end
+
   private
 
   def get_json(_url, params)
-    response = conn.get("youtube/v3/videos", params)
+    response = conn.get(_url, params)
     JSON.parse(response.body, symbolize_names: true)
   end
 
--- spec/services/youtube_service_spec.rb
+++ spec/services/youtube_service_spec.rb
@@ -11,5 +11,32 @@ describe 'YouTube service successfully makes api call' do
     expect(video).to have_key :pageInfo
   end
 
+  it 'can fetch json of a playslists videos', :vcr do
 
+    playlist_id = 'PL1Y67f0xPzdOty2NDYKTRtxeoxvW1mAXu'
+    service = YoutubeService.new
+    playlist = service.playlist_videos_info(playlist_id)
+
+    expect(playlist).to be_a(Hash)
+    expect(playlist[:items]).to be_a(Array)
+    expect(playlist[:items][0]).to be_a(Hash)
+    expect(playlist[:items][0][:snippet]).to have_key :title
+    expect(playlist[:items][0][:snippet]).to have_key :description
+    expect(playlist[:items][0][:snippet]).to have_key :thumbnails
+    expect(playlist[:items][0][:snippet][:resourceId]).to have_key :videoId
+    expect(playlist[:items][0][:snippet]).to have_key :position
+  end
+
+  it 'can fetch json of a playslist tutorial info', :vcr do
+
+    playlist_id = 'PL1Y67f0xPzdOty2NDYKTRtxeoxvW1mAXu'
+    service = YoutubeService.new
+    playlist = service.playlist_tutorial_info(playlist_id)
+
+    expect(playlist[:items]).to be_a(Array)
+    expect(playlist[:items][0][:snippet]).to have_key :title
+    expect(playlist[:items][0][:snippet]).to have_key :description
+    expect(playlist[:items][0][:snippet]).to have_key :thumbnails
+
+  end
 end
