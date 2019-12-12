0ed6a7c8d9ebd6bf5810c9be62bf8841a83cd465
Alice Post <alicep2322@gmail.com>
2019-12-11 18:17:02 -0700

    Add user method to see if the user is a follower/ing and makes arrays the handle

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,20 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friends, through: :friendships
   has_secure_password
+
+  def user_followers
+    return [] unless token
+    facade = UserFacade.new(self)
+    facade.followers.map do |follower|
+      follower.handle
+    end
+  end
+
+  def user_followings
+    return [] unless token
+    facade = UserFacade.new(self)
+    facade.followings.map do |follower|
+      follower.handle
+    end
+  end
 end
